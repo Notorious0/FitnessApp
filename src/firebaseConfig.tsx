1445391816f54d489408d36cadd68b7e18c29d59
Notorious0 <shalitonur@gmail.com>
2025-04-13 11:44:27 +0300

    Update firebaseConfig.tsx

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -3,14 +3,7 @@ import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBr2S-zqSDmngYgiEP40tpGKa9Z6bIKdK4",
-    authDomain: "fitness-f2345.firebaseapp.com",
-    projectId: "fitness-f2345",
-    storageBucket: "fitness-f2345.firebasestorage.app",
-    messagingSenderId: "419832636481",
-    appId: "1:419832636481:web:cbe466d62a08e325fa12c0",
-    measurementId: "G-Z0YK5KDQR5"
-  };
+}
 
 
   const app = initializeApp(firebaseConfig);
